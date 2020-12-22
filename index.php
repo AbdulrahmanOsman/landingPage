bce2f13eba00116e4d35cbade9143258449ef7b6
AbdulrahmanOsman <abdulrahmanosman26@gmail.com>
2020-12-22 11:09:20 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Glegoo:wght@400;700&display=swap"> 
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css?v=<?php echo time(); ?>">
+</head>
+<body>
+
+
+<div class="header">
+    <div class="siteName">
+        <i class="fa fa-bars" aria-hidden="true">
+            <ul>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Menu</a></li>
+                <li><a href="#">Location</a></li>
+                <li><a href="#">Order Now</a></li>
+            </ul>
+        </i>
+        <span>Tastemake<sup>TM</sup> </span>
+
+    </div>
+
+    <div class="transprancyBox">
+        <div class="blur"></div>
+        <div class="menu">
+            <h5>15 MINS | EASY</h5>
+            <p class="cate">Chocolate</p>
+            <p class="cate">Blueberry</p>
+            <p class="cate">Pancakes</p>
+            <p class="description">Lorem ipsum dolor sit amet consec adipisicing elit. Quas quidem, aliquid, inventore obcaecati debitis.</p>
+        </div>
+
+        <div class="author">
+            <img src="images/profilePic.jpg" alt="">
+            <span>Abdulrhaman Osman</span> 
+        </div>
+    </div>
+</div>    
+
+</body>
+</html>
